be1b984ffc0447a889906c446ef2f45114a3b123
George Hazlewood <george.hazlewood@acquia.com>
2018-07-17 13:05:46 +0100

    Allow policies and profiles to be loaded from ~.drutiny home directory

--- src/PolicySource/LocalFs.php
+++ src/PolicySource/LocalFs.php
@@ -9,55 +9,58 @@ use Drutiny\Policy;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Yaml\Yaml;
 
-class LocalFs implements PolicySourceInterface {
+class LocalFs implements PolicySourceInterface
+{
 
   /**
    * {@inheritdoc}
    */
-  public function getName()
-  {
-    return 'localfs';
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getList()
-  {
-    $cache = Container::cache($this->getName())->getItem('list');
-    if ($cache->isHit()) {
-      return $cache->get();
+    public function getName()
+    {
+        return 'localfs';
     }
-    $finder = new Finder();
-    $finder->files()
-      ->in('.')
-      ->name('*.policy.yml');
 
-    $list = [];
-    foreach ($finder as $file) {
-      $policy = Yaml::parse(file_get_contents($file->getRealPath()));
-      $policy['filepath'] = $file->getRealPath();
-      $list[$policy['name']] = $policy;
+  /**
+   * {@inheritdoc}
+   */
+    public function getList()
+    {
+        $cache = Container::cache($this->getName())->getItem('list');
+        if ($cache->isHit()) {
+            return $cache->get();
+        }
+
+        $directories = array('.', getenv('HOME') . '/.drutiny/');
+        $finder = new Finder();
+        $finder->files()
+        ->in($directories)
+        ->name('*.policy.yml');
+
+        $list = [];
+        foreach ($finder as $file) {
+            $policy = Yaml::parse(file_get_contents($file->getRealPath()));
+            $policy['filepath'] = $file->getRealPath();
+            $list[$policy['name']] = $policy;
+        }
+        Container::cache($this->getName())->save(
+            $cache->set($list)->expiresAfter(3600)
+        );
+        return $list;
     }
-    Container::cache($this->getName())->save(
-      $cache->set($list)->expiresAfter(3600)
-    );
-    return $list;
-  }
 
   /**
    * {@inheritdoc}
    */
-  public function load(array $definition)
-  {
-    return new Policy($definition);
-  }
+    public function load(array $definition)
+    {
+        return new Policy($definition);
+    }
 
   /**
    * {@inheritdoc}
    */
-  public function getWeight()
-  {
-    return -10;
-  }
+    public function getWeight()
+    {
+        return -10;
+    }
 }
--- src/Profile/ProfileSourceLocalFs.php
+++ src/Profile/ProfileSourceLocalFs.php
@@ -9,58 +9,61 @@ use Drutiny\Profile;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Yaml\Yaml;
 
-class ProfileSourceLocalFs implements ProfileSourceInterface {
+class ProfileSourceLocalFs implements ProfileSourceInterface
+{
 
   /**
    * {@inheritdoc}
    */
-  public function getName()
-  {
-    return 'localfs';
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getList()
-  {
-    $cache = Container::cache($this->getName())->getItem('profiles');
-    if ($cache->isHit()) {
-      return $cache->get();
+    public function getName()
+    {
+        return 'localfs';
     }
-    $finder = new Finder();
-    $finder->files()
-      ->in('.')
-      ->name('*.profile.yml');
 
-    $list = [];
-    foreach ($finder as $file) {
-      $name = str_replace('.profile.yml', '', pathinfo($file->getRealPath(), PATHINFO_BASENAME));
-      $profile = Yaml::parse(file_get_contents($file->getRealPath()));
-      $profile['filepath'] = $file->getRealPath();
-      $profile['name'] = $name;
-      unset($profile['format']);
-      $list[$name] = $profile;
+  /**
+   * {@inheritdoc}
+   */
+    public function getList()
+    {
+        $cache = Container::cache($this->getName())->getItem('profiles');
+        if ($cache->isHit()) {
+            return $cache->get();
+        }
+
+        $directories = array('.', getenv('HOME') . '/.drutiny/');
+        $finder = new Finder();
+        $finder->files()
+        ->in($directories)
+        ->name('*.profile.yml');
+
+        $list = [];
+        foreach ($finder as $file) {
+            $name = str_replace('.profile.yml', '', pathinfo($file->getRealPath(), PATHINFO_BASENAME));
+            $profile = Yaml::parse(file_get_contents($file->getRealPath()));
+            $profile['filepath'] = $file->getRealPath();
+            $profile['name'] = $name;
+            unset($profile['format']);
+            $list[$name] = $profile;
+        }
+        Container::cache($this->getName())->save(
+            $cache->set($list)->expiresAfter(3600)
+        );
+        return $list;
     }
-    Container::cache($this->getName())->save(
-      $cache->set($list)->expiresAfter(3600)
-    );
-    return $list;
-  }
 
   /**
    * {@inheritdoc}
    */
-  public function load(array $definition)
-  {
-    return Profile::loadFromFile($definition['filepath']);
-  }
+    public function load(array $definition)
+    {
+        return Profile::loadFromFile($definition['filepath']);
+    }
 
   /**
    * {@inheritdoc}
    */
-  public function getWeight()
-  {
-    return -10;
-  }
+    public function getWeight()
+    {
+        return -10;
+    }
 }
