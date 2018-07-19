fd29d5fe5b30204bd4a2a786b8947600dfeff6c1
George Hazlewood <george.hazlewood@acquia.com>
2018-07-19 09:13:59 +0100

    Fix whitespace issue

--- src/Profile/ProfileSourceLocalFs.php
+++ src/Profile/ProfileSourceLocalFs.php
@@ -9,8 +9,7 @@ use Drutiny\Profile;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Yaml\Yaml;
 
-class ProfileSourceLocalFs implements ProfileSourceInterface
-{
+class ProfileSourceLocalFs implements ProfileSourceInterface {
 
   /**
    * {@inheritdoc}
