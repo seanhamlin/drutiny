3a252d4cec017dbf0e8fa4e1da7f8b5124c4a129
George Hazlewood <george.hazlewood@acquia.com>
2018-07-20 12:05:45 +0100

    Revert "Allow policies and profiles to be loaded from ~.drutiny home directory"
    This reverts commit be1b984ffc0447a889906c446ef2f45114a3b123.

--- src/PolicySource/LocalFs.php
+++ src/PolicySource/LocalFs.php
@@ -14,52 +14,50 @@ class LocalFs implements PolicySourceInterface {
   /**
    * {@inheritdoc}
    */
-    public function getName()
-    {
-        return 'localfs';
-    }
+  public function getName()
+  {
+    return 'localfs';
+  }
 
   /**
    * {@inheritdoc}
    */
-    public function getList()
-    {
-        $cache = Container::cache($this->getName())->getItem('list');
-        if ($cache->isHit()) {
-            return $cache->get();
-        }
-
-        $directories = array('.', getenv('HOME') . '/.drutiny/');
-        $finder = new Finder();
-        $finder->files()
-        ->in($directories)
-        ->name('*.policy.yml');
-
-        $list = [];
-        foreach ($finder as $file) {
-            $policy = Yaml::parse(file_get_contents($file->getRealPath()));
-            $policy['filepath'] = $file->getRealPath();
-            $list[$policy['name']] = $policy;
-        }
-        Container::cache($this->getName())->save(
-            $cache->set($list)->expiresAfter(3600)
-        );
-        return $list;
+  public function getList()
+  {
+    $cache = Container::cache($this->getName())->getItem('list');
+    if ($cache->isHit()) {
+      return $cache->get();
     }
+    $finder = new Finder();
+    $finder->files()
+      ->in('.')
+      ->name('*.policy.yml');
+
+    $list = [];
+    foreach ($finder as $file) {
+      $policy = Yaml::parse(file_get_contents($file->getRealPath()));
+      $policy['filepath'] = $file->getRealPath();
+      $list[$policy['name']] = $policy;
+    }
+    Container::cache($this->getName())->save(
+      $cache->set($list)->expiresAfter(3600)
+    );
+    return $list;
+  }
 
   /**
    * {@inheritdoc}
    */
-    public function load(array $definition)
-    {
-        return new Policy($definition);
-    }
+  public function load(array $definition)
+  {
+    return new Policy($definition);
+  }
 
   /**
    * {@inheritdoc}
    */
-    public function getWeight()
-    {
-        return -10;
-    }
+  public function getWeight()
+  {
+    return -10;
+  }
 }
